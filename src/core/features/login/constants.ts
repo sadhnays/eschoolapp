8ff0064e212e6b2051e06fffa69a818e09fd2694
unknown <ysadhna443@gmail.com>
2025-03-11 23:22:17 +0530

    feedback2

--- src/core/features/login/constants.ts
+++ src/core/features/login/constants.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export const GET_STARTED_URL = 'https://moodle.com';
+export const GET_STARTED_URL = 'https://staging.esecondaryschool.com';
 export const ONBOARDING_DONE = 'onboarding_done';
 export const FAQ_QRCODE_INFO_DONE = 'qrcode_info_done';
 export const FAQ_URL_IMAGE_HTML = '<img src="assets/img/login/faq_url.png" role="presentation" alt="">';
